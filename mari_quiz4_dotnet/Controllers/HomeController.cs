2077347b6b2a5c7964ff7f658de1ada0356f6c45
Mariya Ann Moncy <mmoncy1@my.centennialcollege.ca>
2025-04-10 14:09:50 -0400

    Initial commit

---
+++ mari_quiz4_dotnet/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mari_quiz4_dotnet.Models;
+
+namespace mari_quiz4_dotnet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+        ViewBag.Message = "Database access has already been done on April 1, 2025 by Mariya";
+
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
